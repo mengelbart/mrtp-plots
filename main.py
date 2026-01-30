7fdef757fee177d56b52ab35b761518c22d89c26
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-01-30 20:26:22 +0100

    Add alternative parse/plot implementations

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
 import argparse
-import asyncio
 
 from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor
 
-import matplotlib.pyplot as plt
-import pandas as pd
-
-import parsers
+import parquetizer
 import plotters
+import plot2
 import html_generator
-import serializers
 import plot_version_comparison
 import video_quality
 
@@ -150,111 +147,52 @@ plots = [
 ]
 
 
-async def parse_file(input, out_dir, ref_time=None):
-    path = Path(input)
-    if path.name in ['config.json', 'tc.log', 'receiver.stderr.log', 'sender.stderr.log']:
-        df = parsers.parse_json_log(input)
-        serializers.write_feather(
-            df, Path(out_dir) / Path(input).with_suffix('.feather').name)
-    if path.name in ['sender.qlog', 'receiver.qlog']:
-        df = parsers.parse_quic_qlog(input)
-        serializers.write_feather(
-            df, Path(out_dir) / Path(input).with_suffix('.feather').name)
-    if path.name in ['sender.roq.qlog']:
-        df = parsers.parse_roq_qlog(input)
-        serializers.write_feather(
-            df, Path(out_dir) / Path(input).with_suffix('.feather').name)
-    if path.name in ['sender.stderr.log']:
-        df = parsers.parse_pion_sctp_log(input, ref_time)
-        serializers.write_feather(
-            df, Path(out_dir) / Path(input).with_suffix('.sctp.feather').name)
-    if path.suffix == '.pcap':
-        rtp, rtcp, dtls = await parsers.parse_pcap(input)
-
-        if not rtp.empty:
-            serializers.write_feather(
-                rtp, Path(out_dir) / Path(Path(input).stem + '.rtp.feather'))
-        if not rtcp.empty:
-            serializers.write_feather(
-                rtcp, Path(out_dir) / Path(Path(input).stem + '.rtcp.feather'))
-        if not dtls.empty:
-            serializers.write_feather(
-                dtls, Path(out_dir) / Path(Path(input).stem + '.dtls.feather'))
-    if path.name in ['video.quality.csv', 'lost_frames.csv']:
-        df = pd.read_csv(input)
-        serializers.write_feather(
-            df, Path(out_dir) / Path(Path(input).stem + '.feather'))
+def parquetize(input, output):
+    try:
+        parquetizer.Parquetizer(input, output).parquetize()
+    except Exception as e:
+        print(f'failed to parquetize: {input} -> {output}: {e}')
 
 
-async def parse_config(input_dir):
-    """parses config without saving it"""
-    config_path = Path(input_dir) / Path('config.json')
-    if not config_path.is_file():
-        raise FileNotFoundError(f'config.json not found in {input_dir}')
-
-    df = parsers.parse_json_log_no_convert(config_path)
-
-    return df
+def parquetize_cmd(args):
+    if not args.sequential:
+        run_parallel(parquetize, args.input, args.output)
+    else:
+        for input in Path(args.input).iterdir():
+            output = Path(args.output) / Path(args.input).name / input.name
+            parquetize(input, output)
 
 
-async def parse_all_cmd(args):
-    dir = Path(args.input)
-
-    # Parse config.json to get timezone
-    config = await parse_config(dir)
-    ref = pd.Timestamp(config['time'][0])
-
-    for file in dir.iterdir():
-        if file.is_file():
-            await parse_file(file, args.output, ref_time=ref)
+def plot(input, output):
+    try:
+        plot2.Plotter(input, output).plot()
+    except Exception as e:
+        print(f'failed to plot: {input} -> {output}: {e}')
 
 
-async def parse_cmd(args):
-    await parse_file(args.input, args.output)
+def plot_cmd(args):
+    if not args.sequential:
+        run_parallel(plot, args.input, args.output)
+    else:
+        for input in Path(args.input).iterdir():
+            output = Path(args.output) / Path(args.input).name / input.name
+            plot(input, output)
 
 
-async def plot_cmd(args):
-    config_feather = Path(args.input) / Path('config.feather')
-    config = serializers.read_feather(config_feather)
-    start_time = pd.Timestamp(config['time'][0])
+def run_parallel(func, input, output):
+    inputs = []
+    outputs = []
+    for subdir_in in Path(input).iterdir():
+        if subdir_in.is_dir():
+            inputs.append(subdir_in)
+            subdir_out = Path(output) / Path(input).name / subdir_in.name
+            outputs.append(subdir_out)
 
-    for title, func, num_rows, num_column, files, out_name in plots:
-        fig_height = 3*num_rows
-        fig, ax = plt.subplots(
-            nrows=num_rows, ncols=num_column, figsize=(8, fig_height), sharex=True)
-        paths = [Path(args.input) / Path(f) for f in files]
-        if all(p.is_file() for p in paths):
-            dfs = [serializers.read_feather(p) for p in paths]
-            plotted = func(ax, start_time, *dfs)
-            if not plotted:
-                print(f'dropping empty plot {func.__name__}')
-                plt.close(fig)
-                continue
-        else:
-            missing = [str(p) for p in paths if not p.is_file()]
-            print(
-                f'skipping plot {func.__name__} due to missing dependencies {', '.join(missing)}')
-            plt.close(fig)
-            continue
-
-        if num_column > 1 or num_rows > 1:
-            fig.suptitle(title)
-            axes = ax.flat if hasattr(ax, 'flat') else (
-                ax if isinstance(ax, list) else [ax])
-            for axis in axes:
-                if axis.get_legend() is not None:
-                    axis.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncols=4, mode="expand", borderaxespad=0.)
-
-        else:
-            ax.set_title(title)
-        fig.autofmt_xdate()
-        fig.tight_layout()
-        # fig.subplots_adjust(hspace=0.3)
-        fig.savefig(Path(args.output) / Path(out_name), dpi=300)
-        plt.close(fig)
+    with ProcessPoolExecutor() as executor:
+        list(executor.map(func, inputs, outputs))
 
 
-async def generate_cmd(args):
+def generate_cmd(args):
     html_generator.generate_html(args.input)
 
 
@@ -271,7 +209,7 @@ async def plot_combis_cmd(args):
             args.input, args.output)
 
 
-async def calc_video_metrics(args):
+def calc_video_metrics(args):
     video_quality.calculate_quality_metrics(
         args.reference, args.input, args.output)
 
@@ -280,28 +218,27 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     subparsers = parser.add_subparsers(help='sub-command help', required=True)
+    parser.add_argument('-s', '--sequential',
+                        action=argparse.BooleanOptionalAction, default=False,
+                        help='run sequential instead of parallel')
 
-    parse = subparsers.add_parser(
-        'parse', help='parses log files, converts them to a data frame and serializes the data frame as feather file(s)')
-    parse.add_argument('-i', '--input', help='input log file', required=True)
-    parse.add_argument(
-        '-o', '--output', help='output directory', required=True)
-    parse.set_defaults(func=parse_cmd)
-
-    parse_all = subparsers.add_parser(
-        'parse-all', help='parse all files in a directory by trying to find a suitable parser for each file by name')
-    parse_all.add_argument(
+    parquetize = subparsers.add_parser('parquetize', help='alternative to '
+                                       'parse parses data and stores it in a '
+                                       'single parquet file per experiment')
+    parquetize.add_argument(
         '-i', '--input', help='input directory', required=True)
-    parse_all.add_argument(
+    parquetize.add_argument(
         '-o', '--output', help='output directory', required=True)
-    parse_all.set_defaults(func=parse_all_cmd)
+    parquetize.set_defaults(func=parquetize_cmd)
 
-    plot = subparsers.add_parser(
-        'plot', help='reads a data frame from a feather file and creates plots')
+    plot = subparsers.add_parser('plot', help='reads a data frame from a '
+                                 'feather file and creates plots')
     plot.add_argument(
         '-i', '--input', help='input directory', required=True)
     plot.add_argument(
         '-o', '--output', help='output directory', required=True)
+    plot.add_argument('-f', '--format', default='png',
+                      help='output file format, e.g., \'pdf\', or \'png\'')
     plot.set_defaults(func=plot_cmd)
 
     generate = subparsers.add_parser(
@@ -311,28 +248,19 @@ def main():
     generate.add_argument(
         '-i', '--input', help='input directory', required=True)
 
-    plot_combis = subparsers.add_parser(
-        'plot-combis', help='creates a combined plot for each test case')
-    plot_combis.add_argument(
-        '-i', '--input', help='input directory with test cases', required=True)
-    plot_combis.add_argument(
-        '-o', '--output', help='output directory for plots', required=True)
-    plot_combis.add_argument('-m', '--mode', choices=['version', 'link', "default", "avgs"], default='version',
-                             help='comparison mode: "version" combines by test version (e.g. each webrtc-gcc test), "link" combines by link type (e.g. each static test), "default" uses predefined combinations, "avgs" calculates some average metrics over all test cases')
-    plot_combis.set_defaults(func=plot_combis_cmd)
-
     video_qm = subparsers.add_parser(
         'video-quality', help='caluculate video quality metrics using ffmpeg')
     video_qm.add_argument(
         '-r', '--reference', help='reference video', required=True)
-    video_qm.add_argument(
-        '-i', '--input', help='folder that contains the test results, including the video with the name out.y4m', required=True)
-    video_qm.add_argument(
-        '-o', '--output', help='output directory for result csv\'s', required=True)
+    video_qm.add_argument('-i', '--input', required=True,
+                          help='folder that contains the test results, '
+                               'including the video with the name out.y4m')
+    video_qm.add_argument('-o', '--output', required=True,
+                          help='output directory for result csv\'s')
     video_qm.set_defaults(func=calc_video_metrics)
 
     args = parser.parse_args()
-    asyncio.run(args.func(args))
+    args.func(args)
 
 
 if __name__ == "__main__":
